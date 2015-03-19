c5a13f02f104c5ce7d9a1e0e5151e390b0694893
AlexSheer <alex_sheer@inbox.lv>
2015-03-20 01:35:30 +0200

    Create permissions_minical.php

---
+++ language/ru/permissions_minical.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Extension - Mini Calendar
+* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_U_ADD_EVENT'		=> 'Может добавлять события',
+));
