7d8291d6394c1173e7dd3fea6239ae550f913624
AlexSheer <alex_sheer@inbox.lv>
2015-03-20 01:33:45 +0200

    Create minical.php

---
+++ language/ru/minical.php
@@ -0,0 +1,34 @@
+<?php
+/**
+*
+* @package phpBB Extension - Nav Links In Header
+* @copyright (c) 2015 Sheer
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'EVENT_TITLE'		=> 'Заголовок события',
+	'START_DATE'		=> 'Дата наступления события',
+	'END_DATE'			=> 'Дата окончаниия события',
+	'ADD_EVENT_EXPLAIN' => 'Если вы не хотите добавлять событие, оставьте это поле пустым.',
+	'SELECT_DATE'		=> 'Выбрать дату',
+	'END_DATE_ERROR'	=> 'Дата окончания события не может быть меньше даты наступления события.',
+	'START_DATE_ERROR'	=> 'Дата события не может быть меньше текущей.',
+	'TITLE_TOO_SHORT'	=> 'Заголовок события слишком короткий.',
+	'DELETE_EVENT'		=> 'Удалить событие',
+	'ADVANCED_FORM'		=> 'расширенный режим',
+));
