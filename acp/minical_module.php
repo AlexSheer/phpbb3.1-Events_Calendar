2be59ee1bee8ed84f81fc0153f0db6d73d9379cb
AlexSheer <alex_sheer@inbox.lv>
2015-03-30 13:21:23 +0300

    Update minical_module.php

--- acp/minical_module.php
+++ acp/minical_module.php
@@ -21,6 +21,18 @@ class minical_module
 		$this->page_title = $user->lang('ACP_MINICAL');
 		$action		= request_var('action', '');
 
+		$forums = explode(',', $config['minical_forums']);
+
+		$minical_forums	= $request->variable('forum_id', $forums);
+
+		$forum_list = make_forum_select(false, false, true, true, true, false, true);
+		$s_forum_options = '';
+		foreach($forum_list as $key => $value)
+		{
+			$selected = (in_array($value['forum_id'], $forums)) ? true : false;
+			$s_forum_options .='<option value="' . $value['forum_id'] . '"' . (($selected) ? ' selected="selected"' : '') . (($value['disabled']) ? ' disabled="disabled" class="disabled-option"' : '') . '>' . $value['padding'] . $value['forum_name'] . '</option value>';
+		}
+
 		add_form_key('sheer/minical');
 
 		if ($request->is_set_post('submit'))
@@ -29,17 +41,17 @@ class minical_module
 			{
 				trigger_error('FORM_INVALID');
 			}
+			$config->set('minical_forums', implode(',', $minical_forums));
 			$config->set('minical_enable', $request->variable('minical_enable', 0));
-			$config->set('minical_forums', $request->variable('minical_forums', ''));
 
 			trigger_error($user->lang['CONFIG_UPDATED'] . adm_back_link($this->u_action));
 		}
 
 		$template->assign_vars(array(
 			'U_ACTION'			=> $this->u_action,
-			'MINICAL_FORUMS'	=> $config['minical_forums'],
 			'S_CHECKED_DISABLE'	=> (!$config['minical_enable']) ? ' checked="checked" ' : '',
 			'S_CHECKED_ENABLE'	=> ($config['minical_enable']) ? ' checked="checked" ' : '',
+			'S_FORUM_OPTIONS'	=> $s_forum_options,
 		));
 	}
 }
